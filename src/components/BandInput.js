8910ff35a13cebd57fb61ad13b0c1b8c3a694e07
megfly <mflynn333@gmail.com>
2020-12-24 14:39:09 -0500

    Done.

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -1,14 +1,48 @@
 // Add BandInput component
 import React, { Component } from 'react'
 
+//functional components are stateless, if we want to use stte we need to change to class
 class BandInput extends Component {
+
+  //initial state with text key set to empty string...
+state = {
+  //setup controlled form wiht internal state
+  name: ''
+}
+
+handleOnChange = event => {
+  console.log(event.target.value)
+  this.setState({
+    name: event.target.value
+  })
+}
+
+handleOnSubmit = event => {
+  event.preventDefault()
+  this.props.addBand(this.state)
+  this.setState({
+    name: ''
+  })
+}
+
   render() {
     return(
-      <div>
-        Band Input
+      <div className="container">
+        <form onSubmit={this.handleOnSubmit} className="band-input">
+          {/* band input has a text input field */}
+          <input
+            type="text"
+            onChange={this.handleOnChange}
+            name="band-input"
+            value={this.state.name}
+          />
+          <input type="Submit" />
+        </form>
       </div>
     )
   }
 }
+
+
 
 export default BandInput
--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,13 +1,51 @@
 import React, { Component } from 'react'
+//connecting to the global state store so it knows bands
+import { connect } from 'react-redux';
+//importing bandinput in order to pass data to it
+import BandInput from '../components/BandInput'
 
 class BandsContainer extends Component {
+
   render() {
+    console.log("props is", this.props)
+
+    let bandsList = this.props.bands.map((band, index) => {
+      return <li key={index}>{band.name}</li>
+    })
+
     return(
+    // The dispatched action required to initiate this submit btn update should 
+    //be set up in BandsContainer and passed down as the prop addBand to BandInput.
       <div>
-        BandsContainer
+        {/* BandsContainer */}
+        <BandInput addBand={this.props.addBand} />
+        <br />
+        {bandsList}
       </div>
     )
   }
 }
 
-export default BandsContainer
+//shopping at the store 
+const mapStateToProps = state => {
+  //must return object 
+  return {
+    bands: state.bands
+  }
+}
+
+//actions?
+const mapDispatchToProps = dispatch => {
+  return {
+    addBand: band => {
+      dispatch({ type: "ADD_BAND", band })
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BandsContainer);
+
+//This component should act as your connection to the Redux store and should be a 
+//child of the App component. Any values we want to use from Redux as well as any 
+//actions we want to dispatch should be set 
+//up and available in BandsContainer so they can be passed down to other components.
